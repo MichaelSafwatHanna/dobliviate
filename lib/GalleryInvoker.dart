a3e8e40523ada2393893096aac8f9204448cd1aa
MichaelSafwatHanna <michaelsafwat.hanna@gmail.com>
2020-06-12 19:05:07 +0200

    Refactor Image Info Mapping

--- lib/GalleryInvoker.dart
+++ lib/GalleryInvoker.dart
@@ -1,3 +1,4 @@
+import 'package:dobliviate/Image.dart';
 import 'package:flutter/services.dart';
 
 class GalleryPlatformInvoker {
@@ -6,6 +7,15 @@ class GalleryPlatformInvoker {
   static Future<Object> get getTodayImages async {
     Map<dynamic, dynamic> todayImagesMap =
         await _channel.invokeMethod('getTodayImages');
-    return todayImagesMap;
+    List<Image> images = List();
+    int imagesCount = (todayImagesMap["URI"] as List).length;
+    for (int i = 0; i < imagesCount; i++) {
+      images.add(Image(
+          (todayImagesMap["URI"] as List)[i],
+          (todayImagesMap["DISPLAY_NAME"] as List)[i],
+          DateTime.parse((todayImagesMap["DATE_ADDED"] as List)[i]),
+          (todayImagesMap["TITLE"] as List)[i]));
+    }
+    return images;
   }
 }
---
+++ lib/Image.dart
@@ -0,0 +1,14 @@
+class Image{
+  String _uri;
+  String _displayName;
+  DateTime _dateAdded;
+  String _title;
+
+  Image(this._uri, this._displayName, this._dateAdded, this._title);
+
+  String get title => _title;
+  DateTime get dateAdded => _dateAdded;
+  String get displayName => _displayName;
+  String get uri => _uri;
+
+}
